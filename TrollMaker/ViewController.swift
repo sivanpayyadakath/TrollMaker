0fb81ee099f713ffc02dfaa6d00d10d0f5aa3e8c
sivanpayyadakath <sivan.payyadakath@gmail.com>
2019-04-26 11:31:22 +0900

    adding_texts_and_keyboard-show-hide

--- TrollMaker/ViewController.swift
+++ TrollMaker/ViewController.swift
@@ -8,21 +8,81 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     
     @IBOutlet weak var selectedImage: UIImageView!
     @IBOutlet weak var imagePicker: UIBarButtonItem!
+    @IBOutlet weak var cameraImagePicker: UIBarButtonItem!
+    @IBOutlet weak var topField: UITextField!
+    @IBOutlet weak var bottomField: UITextField!
+    
+    let trollTextAttribute: [NSAttributedString.Key: Any] = [
+        NSAttributedString.Key.strokeColor: UIColor.black,
+        NSAttributedString.Key.foregroundColor: UIColor.white,
+        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSAttributedString.Key.strokeWidth: 50
+        
+    ]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        topField.delegate = self
+        bottomField.delegate = self
+        topField.isHidden = true
+        bottomField.isHidden = true
         // Do any additional setup after loading the view.
+        
+        //topField.defaultTextAttributes: trollTextAttribute
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
 
+    func subscribeToKeyboardNotifications(){
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    func unsubscribeFromKeyboardNotifications(){
+        NotificationCenter.default.removeObserver(self,name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification){
+        view.frame.origin.y -= getKeyboardHeight(notification)
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification){
+        view.frame.origin.y = 0
+    }
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
+    
     @IBAction func photoPicked(_ sender: Any) {
         let pickerController = UIImagePickerController()
         pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func cameraPhotoPicked(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .camera
         present(pickerController, animated: true, completion: nil)
     }
     
@@ -31,7 +91,19 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             selectedImage.image = image
         }
         dismiss(animated: true, completion: nil)
+        topField.isHidden = false
+        bottomField.isHidden = false
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.text = ""
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        self.view.endEditing(true)
         
+        return true
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
